55a0cd982ea7dc1b181beeff720c3c6863e32f8e
qlcx <1136425019@qq.com>
2017-07-06 18:07:39 +0800

    添加sider

--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -2,9 +2,48 @@ import React, { Component } from 'react'
 
 import styles from './Layout.css'
 
-const Layout = props => {
-  return(
-    <main>
+const topicTypes = [{
+  type: 'all', icon: 'icon-quanbu', name: '全部'
+},{
+  type: 'best', icon: 'icon-huo', name: '精华'
+},{
+  type: 'share', icon: 'icon-fenxiang', name: '分享'
+},{
+  type: 'answer', icon: 'icon-wenda', name: '问答'  
+},{
+  type: 'recruit', icon: 'icon-zhaopin', name: '招聘'  
+},{
+  type: 'about', icon: 'icon-guanyu', name: '关于'  
+}]
+
+export default class Layout extends Component {
+  constructor(props) {
+    super(props)
+
+    this.renderSider = this.renderSider.bind(this)
+
+    this.state = {
+      currentType: 'all',
+    }
+  }
+
+  render() {
+    return (
+      <main>
+        {this.renderHeader()}
+        
+        <div className={styles.container}>
+          {this.renderSider()}
+          <div className={styles.content}>
+            {this.props.children}
+          </div>
+        </div>
+      </main>
+    )
+  }
+
+  renderHeader() {
+    return (
       <div className={styles.header}>
         <div className={styles.tmTbod}>
           <img className={styles.imgLogo} src='https://cnodejs.org/public/images/cnodejs_light.svg' />
@@ -13,32 +52,33 @@ const Layout = props => {
             <i className='iconfont icon-menu' />
           </a>
         </div>
-        
-        {/* <nav className={styles.headerNav}>
-          <ul>
-            <li><a href='#'>全部</a></li>
-            <li><a href='#'>精华</a></li>
-            <li><a href='#'>分享</a></li>
-            <li><a href='#'>问答</a></li>
-            <li><a href='#'>招聘</a></li>
-            <li><a href='#'>关于</a></li>
-            
-            <li><a href='#'>登录</a></li>
-          </ul>
-        </nav> */}
+
         <a className={styles.avatar} onClick={() => console.log('login')}>
           <i className='iconfont icon-login' />
         </a>
       </div>
-      
-      <div className={styles.container}>
-        {props.children}
-      </div>
-      
-      <div>
-      </div>
-    </main>
-  )
-}
+    )
+  }
 
-export default Layout
+  renderSider() {
+    return (
+      <div className={styles.sider}>
+        <ul>
+          {
+            topicTypes.map(data => {
+              return <li key={data.type}>
+                <a 
+                  href='#'
+                  className={data.type === this.state.currentType ? styles.currentType : undefined} 
+                  onClick={() => this.setState({currentType: data.type})}>
+                  <i className={`iconfont ${data.icon}`} />
+                  <span>{data.name}</span>
+                </a>
+              </li>
+            })
+          }
+        </ul>
+      </div>
+    )
+  }
+}
--- sw-precache-config.js
+++ sw-precache-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   runtimeCaching: [{
-    urlPattern: /^https:\/\/cnodejs\.org\/api\/v1\/topics/,
+    urlPattern: /^https:\/\/cnodejs\.org\/api\/v1/,
     handler: 'networkFirst'
   },{
     urlPattern: /^https:\/\/cnodejs\.org\/public\/images\/cnodejs_light\.svg/,
