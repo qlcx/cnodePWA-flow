45af15d50a4c6c09afe862cafec8e0272af0efc9
qlcx <1136425019@qq.com>
2017-07-18 07:46:11 +0800

    topic details layout

--- src/components/TopicDetailsPage/TopicInfo.js
+++ src/components/TopicDetailsPage/TopicInfo.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react'
+
 import markdownStyle from './github-markdown.css'
+import styles from './TopicInfo.css'
 
 export default class TopicInfo extends Component {
   render() {
 
-    return <div className={markdownStyle['markdown-body']} dangerouslySetInnerHTML={{__html: this.props.content}} />  
+    return <div className={styles.root}>
+      <div className={markdownStyle['markdown-body']} dangerouslySetInnerHTML={{__html: this.props.content}} />
+    </div>
   }
 }
