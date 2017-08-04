0527cba951ad4ea39d499ebf41fdef1ba4f45f5e
qlcx <1136425019@qq.com>
2017-08-04 23:18:42 +0800

    layout 添加静态检查

--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -1,13 +1,43 @@
+//@flow
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import type { Children } from 'react'
 
 import Header from './Header'
 import Sider from './Sider'
 
 import styles from './Layout.css'
 
+type LayoutProps = {
+  children: Children,
+}
+
 export default class Layout extends Component {
-  constructor(props) {
+  state: {
+    menuPosition: number,
+    isShowSider: boolean,
+    isShowHeader: boolean,
+  }
+  props: LayoutProps
+
+  // 处理事件
+  handleMouseDown: (event: MouseEvent) => void
+  handleTouchStart: (event: TouchEvent) => void
+  handleTouchEnd: () => void
+  handleTouchMove: (event: TouchEvent) => void
+  handleResize: () => void
+  handleScroll: () => void
+
+  // 滚动
+  prevScrollPos: number
+  prevScrollDirection: string
+  scrollDirection: string
+  // 触摸移动
+  startX: number
+  // 触摸结束定时器
+  touchEndTimer: number
+
+  constructor(props: LayoutProps) {
     super(props)
 
     // 事件监听
@@ -48,7 +78,7 @@ export default class Layout extends Component {
     // 页面宽度<=600时处理滚动事件
     if (!this.state.isShowSider) {
       // 函数去抖
-      let onScrollTop = document.body.scrollTop
+      let onScrollTop: number = document.body ? document.body.scrollTop : 0
 
       if (this.prevScrollPos && this.prevScrollPos > onScrollTop) {
         this.scrollDirection = 'up'
@@ -78,7 +108,7 @@ export default class Layout extends Component {
   }
 
   // 监听触摸开始事件
-  handleTouchStart(event) {
+  handleTouchStart(event: TouchEvent) {
     // 判断有几根手机
     let touchesCnt = event.changedTouches.length
 
@@ -88,7 +118,7 @@ export default class Layout extends Component {
   }
 
   // 监听触摸结束事件
-  handleTouchEnd(event) {
+  handleTouchEnd() {
     this.touchEndTimer && clearTimeout(this.touchEndTimer)
 
     this.touchEndTimer = setTimeout(() => {
@@ -103,7 +133,7 @@ export default class Layout extends Component {
   }
 
   // 监听触摸移动事件
-  handleTouchMove(event) {
+  handleTouchMove(event: TouchEvent) {
     // 判断有几根手机
     let touchesCnt = event.changedTouches.length
 
@@ -114,8 +144,8 @@ export default class Layout extends Component {
 
       setTimeout(() => {
         // 判断移动方向
-        let endX = event.changedTouches[0].pageX
-        let moveLen = endX - this.startX
+        let endX: number = event.changedTouches[0].pageX
+        let moveLen: number = endX - this.startX
 
         if (moveLen < -30) {
           this.setState({ menuPosition: -200 })
@@ -129,10 +159,10 @@ export default class Layout extends Component {
   }
 
   // 监听鼠标事件
-  handleMouseDown(event) {
-    let e = event || window.event;
-    let scrollX = document.documentElement.scrollLeft || document.body.scrollLeft;
-    let x = e.pageX || e.clientX + scrollX;
+  handleMouseDown(event: MouseEvent) {
+    let e: MouseEvent = event || window.event
+    let scrollX: number = (document.documentElement && document.documentElement.scrollLeft) || (document.body && document.body.scrollLeft) || 1
+    let x: number = e.pageX || e.clientX + scrollX
 
     if (x > 200) {
       this.setState({ menuPosition: -200 })
